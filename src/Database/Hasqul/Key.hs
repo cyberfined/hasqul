49e05632a86ff20f45d5592f927af0dcbfa4758e
cyberfined <cyberfined@protonmail.com>
2021-10-16 00:34:03 +0300

    Make FromJSON and ToJSON instance for Key a

--- src/Database/Hasqul/Key.hs
+++ src/Database/Hasqul/Key.hs
@@ -1,24 +1,18 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Database.Hasqul.Key
     ( Key(..)
     ) where
 
+import Data.Aeson
 import Data.Default.Class
-import Data.Functor.Contravariant ((>$<))
 import Data.Int                   (Int64)
 import Database.Hasqul.Valuable
 import GHC.Generics
 
-import qualified Hasql.Decoders as Dec
-import qualified Hasql.Encoders as Enc
-
 -- Type for primary and foreign key
-newtype Key a = Key { unKey :: Int64 } deriving (Generic, Show, Eq, Ord)
-
-instance Valuable (Key a) where
-    valueDec = Key <$> Dec.int8
-    valueEnc = unKey >$< Enc.int8
-
-instance Default (Key a) where
-    def = Key 0
+newtype Key a = Key { unKey :: Int64 }
+    deriving stock (Generic, Show, Eq, Ord)
+    deriving newtype (Valuable, Default, FromJSON, ToJSON)
